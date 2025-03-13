00b76e6366c57e38619a39acc52f7ae3d17e2c9f
is201spencer <scw01@byu.edu>
2025-03-12 19:38:36 -0600

    Initial

---
+++ backend/Mission10/Mission10/Controllers/BowlerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mission10.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mission10.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BowlerController : ControllerBase
+    {
+        private readonly BowlingLeagueContext _context;
+
+        public BowlerController(BowlingLeagueContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetBowler")]
+        public IActionResult Get()
+        {
+            var bowlers = _context.Bowlers
+                .Include(b => b.Team) // Join with Team table
+                .Where(b => b.Team != null && (b.Team.TeamName == "Marlins" || b.Team.TeamName == "Sharks")) // Filter
+                .Select(b => new
+                {
+                    BowlerId = b.BowlerId,
+                    FullName = $"{b.BowlerFirstName} {b.BowlerMiddleInit} {b.BowlerLastName}".Trim(),
+                    TeamName = b.Team.TeamName,
+                    Address = b.BowlerAddress,
+                    City = b.BowlerCity,
+                    State = b.BowlerState,
+                    Zip = b.BowlerZip,
+                    PhoneNumber = b.BowlerPhoneNumber
+                })
+                .ToList();
+
+            return Ok(bowlers);
+        }
+    }
+}
